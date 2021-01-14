a23945f8336ec71095f7924e2394a99a955ebc4d
Kawaki-san <rtkayy123@gmail.com>
2021-01-15 01:58:12 +0200

    add tailwind

---
+++ src/resources/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WhoWeAre from '../components/WhoWeAre'
+import Hero from '../views/Hero'
+
+function Home() {
+    return (
+        <div>
+            <Hero />
+            <WhoWeAre />
+        </div>
+    )
+}
+
+export default Home
