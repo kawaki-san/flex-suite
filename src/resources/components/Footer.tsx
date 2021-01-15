dc34a75d2bccef0119dc998243001bf40813fab4
Kawaki-san <rtkayy123@gmail.com>
2021-01-15 02:43:48 +0200

    design: home page

--- src/resources/components/Footer.tsx
+++ src/resources/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
 import PhoneEnabledOutlinedIcon from '@material-ui/icons/PhoneEnabledOutlined';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+
 function Footer() {
     return (
         <footer className="text-gray-400 bg-gray-900 body-font">
@@ -96,7 +96,7 @@ function Footer() {
             <div className="bg-gray-800 bg-opacity-75">
                 <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
                     <p className="text-gray-400 text-sm text-center sm:text-left">© 2020 CITS —
-        <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+        <a href="https://www.spectorware.com" className="text-gray-500 ml-1" rel="noopener noreferrer" target="_blank">SpectorWare</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                         <a className="text-gray-400">
--- src/resources/components/WhoWeAre.tsx
+++ src/resources/components/WhoWeAre.tsx
@@ -5,13 +5,15 @@ function WhoWeAre() {
         <section className="text-gray-400 body-font bg-gray-900">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
-                    <h2 className="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1">ROOF PARTY POLAROID</h2>
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Master Cleanse Reliac Heirloom</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+                    <h2 className="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1">WHO WE ARE</h2>
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Get To Know Us</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Corporate Information Technology Solutions, trading as CITS is an ICT consulting firm instituted to provide total end-to-end ICT solutions. CITS has been in business for close to a decade now and is a pure solutions company with a unique approach to assisting its clients to solve their problems.</p>
+                    <br />
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">CITS provides solutions in the form of services and products to clients after aligning them to the clients challenges, budget and industry trends so as to ensure the solution is a value adding one. It is this that has seen CITS client base grow from initially focusing on the banking industry to now include clients in the manufacturing, government, NGO, aviation, insurance, education and transport sectors. </p>
                 </div>
                 <div className="flex flex-wrap">
                     <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Shooting Stars</h2>
+                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Flex Performance</h2>
                         <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
                         <a className="text-indigo-400 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -20,7 +22,7 @@ function WhoWeAre() {
                         </a>
                     </div>
                     <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">The Catalyzer</h2>
+                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Flex CRM</h2>
                         <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
                         <a className="text-indigo-400 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -29,7 +31,7 @@ function WhoWeAre() {
                         </a>
                     </div>
                     <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Neptune</h2>
+                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Flex Payroll</h2>
                         <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
                         <a className="text-indigo-400 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -38,7 +40,7 @@ function WhoWeAre() {
                         </a>
                     </div>
                     <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-800">
-                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Melanchole</h2>
+                        <h2 className="text-lg sm:text-xl text-white font-medium title-font mb-2">Flex Stock and Inventory</h2>
                         <p className="leading-relaxed text-base mb-4">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
                         <a className="text-indigo-400 inline-flex items-center">Learn More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
@@ -47,7 +49,10 @@ function WhoWeAre() {
                         </a>
                     </div>
                 </div>
-                <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <a href="">
+                    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Our Products</button>
+                </a>
+
             </div>
         </section>
     )
