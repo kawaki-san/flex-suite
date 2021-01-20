41d34974f3c1edf276c8e80f2d29c6d75a181f28
Kawaki-san <rtkayy123@gmail.com>
2021-01-20 16:11:47 +0200

    sets default data in modal

--- src/resources/components/EditProductModal.tsx
+++ src/resources/components/EditProductModal.tsx
@@ -33,6 +33,12 @@ function EditProductModal({ modalIsOpen, setModalIsOpen, currentProduct }: { mod
     }
     console.log("My Product is:", currentProduct.path)
 
+
+    const [benefits, setBenefits] = useState([])
+    const [description, setDescription] = useState("")
+    const [summary, setSummary] = useState("")
+
+
     if (product == null) {
         return <div>Loading Data</div>
     } else
@@ -50,7 +56,7 @@ function EditProductModal({ modalIsOpen, setModalIsOpen, currentProduct }: { mod
                             </div>
                             <div className="lg:w-1/2 md:w-2/3 mx-auto">
                                 <div className="flex flex-wrap -m-2">
-                                    <div className="p-2 w-1/2">
+                                    <div className="p-2 w-full">
                                         <div className="relative">
                                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Product Name</label>
                                             <input type="text" id="name" name="name" value={product.product} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
@@ -58,49 +64,29 @@ function EditProductModal({ modalIsOpen, setModalIsOpen, currentProduct }: { mod
                                     </div>
                                     <div className="p-2 w-full">
                                         <div className="relative">
-                                            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Product Summary</label>
-                                            <textarea id="message" name="message" value={product.summary} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                            <label htmlFor="summary" className="leading-7 text-sm text-gray-600">Product Summary</label>
+                                            <textarea id="summary" name="summary" defaultValue={product.summary} onChange={e => setSummary(e.target.value)} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                         </div>
                                     </div>
 
                                     <div className="p-2 w-full">
                                         <div className="relative">
-                                            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Product Details</label>
-                                            <textarea id="message" name="message" value={product.details} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                            <label htmlFor="details" className="leading-7 text-sm text-gray-600">Product Details</label>
+                                            <textarea id="details" name="details" defaultValue={product.details} onChange={e => setDescription(e.target.value)} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                         </div>
                                     </div>
+
                                     <div className="p-2 w-full">
-                                        <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+                                        <div className="relative">
+                                            <label htmlFor="benefits" className="leading-7 text-sm text-gray-600">Product Benefits (Put each benefit on its own line)</label>
+                                            <textarea id="benefits" name="benefits" defaultValue={product.benefit.join("\n")} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        </div>
                                     </div>
-                                    <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                                        <a className="text-blue-500">example@email.com</a>
-                                        <p className="leading-normal my-5">49 Smith St.
-            <br />Saint Cloud, MN 56301
-          </p>
-                                        <span className="inline-flex">
-                                            <a className="text-gray-500">
-                                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                                </svg>
-                                            </a>
-                                            <a className="ml-4 text-gray-500">
-                                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                                                </svg>
-                                            </a>
-                                            <a className="ml-4 text-gray-500">
-                                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                                                </svg>
-                                            </a>
-                                            <a className="ml-4 text-gray-500">
-                                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                                </svg>
-                                            </a>
-                                        </span>
+
+                                    <div className="p-2 w-full">
+                                        <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Save Changes</button>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
