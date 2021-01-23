188731bab8b4a181308a1c62182d3370ca00aaf0
Kawaki-san <rtkayy123@gmail.com>
2021-01-23 20:32:11 +0200

    adds firebase storage

--- src/.firebaseConfig.ts
+++ src/.firebaseConfig.ts
@@ -1,11 +1,12 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  
+   
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
+const storage = firebase.storage();
 
-export { db, auth };
+export { db, auth,storage };
